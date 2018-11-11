770737f67c438a5da7cf397c8460ea2952d2fc2b
Yao Yao <yaoy.cs@gmail.com>
2018-11-10 17:44:29 -0800

    add a bash script to download all input files to run the experiments

---
+++ experiments/CERENKOV2/wget_input_files.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget -q http://files.cgrb.oregonstate.edu/Ramsey_Lab/cerenkov2/osu18_feature_names_categories.txt -O osu18_feature_names_categories.txt
+wget -q http://files.cgrb.oregonstate.edu/Ramsey_Lab/cerenkov2/osu18_features1.1_cerenkov2\(anonymous\).rds -O osu18_features1.1_cerenkov2\(anonymous\).rds
+wget -q http://files.cgrb.oregonstate.edu/Ramsey_Lab/cerenkov2/osu18_intra_locus_dist\(anonymous\).rds -O osu18_intra_locus_dist\(anonymous\).rds
+wget -q http://files.cgrb.oregonstate.edu/Ramsey_Lab/cerenkov2/osu18_snp_coordinates\(anonymous\).rds -O osu18_snp_coordinates\(anonymous\).rds
