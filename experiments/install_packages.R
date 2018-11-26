71ba24afc1f8a5d2fd4cf70c62da7d41b5870096
Yao Yao <yaoy.cs@gmail.com>
2018-11-26 14:22:59 -0800

    add a script to install all packages required by 'machine_learning' and 'experiments' scripts

---
+++ experiments/install_packages.R
@@ -0,0 +1,6 @@
+install.packages(c("plyr", "parallel", "pbapply", "reshape2", "ggplot2", "methods", "aws.ec2", "ranger", "xgboost", "pls", "dplyr", "fitdistrplus", "extrafont", "arrangements")
+
+# Need "ranger" version 0.6.0 specifically
+# Rager Archive: https://cran.r-project.org/src/contrib/Archive/ranger/
+ranger_package_url <- "https://cran.r-project.org/src/contrib/Archive/ranger/ranger_0.6.0.tar.gz"
+install.packages(ranger_package_url, repos=NULL, type="source")
